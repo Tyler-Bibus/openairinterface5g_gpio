886c622ff3a388487cc82577c8d6c2ec808a37a3
tyler-bibus <tylerbibus@hotmail.com>
2025-07-30 14:46:53 -0500

    bugfix: Pulled values from nr_mac directly instead of radio_config

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -803,7 +803,7 @@ static void nr_generate_Msg3_retransmission(module_id_t module_idP,
   int slots_frame = nr_mac->frame_structure.numb_slots_frame;
   uint16_t K2 = tda_info.k2 + get_NTN_Koffset(scc);
   const int sched_frame = (frame + (slot + K2) / slots_frame) % MAX_FRAME_NUMBER;
-  int msg3_slot = nr_mac->msg2_slot + nr_mac->k2_msg3 + 3; // FIXME, confirm that these ptrs work
+  int msg3_slot = nr_mac->radio_config.msg2_slot + nr_mac->radio_config.k2_msg3 + 3; // FIXME, confirm that these ptrs work
   const int sched_slot = msg3_slot % slots_frame;
 
   if (is_ul_slot(sched_slot, &nr_mac->frame_structure)) {
