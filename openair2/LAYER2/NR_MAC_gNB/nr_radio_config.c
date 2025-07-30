bbfa0f848677cc428f4d1f4dfde67281f34c7360
tyler-bibus <tylerbibus@hotmail.com>
2025-07-30 15:53:15 -0500

    pulling k2_msg3 from config rather than calculated.

--- openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
+++ openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
@@ -1126,7 +1126,7 @@ void nr_rrc_config_ul_tda(NR_ServingCellConfigCommon_t *scc, int min_fb_delay, n
     int tdd_period_idx = get_tdd_period_idx(scc->tdd_UL_DL_ConfigurationCommon);
     int nb_periods_per_frame = get_nb_periods_per_frame(tdd_period_idx);
     int nb_slots_per_period = ((1 << mu) * 10) / nb_periods_per_frame;
-    int k2_msg3 = nb_slots_per_period - get_delta_for_k2(mu);
+    int k2_msg3 = config.k2_msg3;
     struct NR_PUSCH_TimeDomainResourceAllocation *puschTdrAllocMsg3 = set_TimeDomainResourceAllocation(k2_msg3, 3, ul_symb);
     if (*puschTdrAllocMsg3->k2 < min_fb_delay)
       *puschTdrAllocMsg3->k2 += nb_slots_per_period;
